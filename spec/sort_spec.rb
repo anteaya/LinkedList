013c4050ca436d472cc6451ee0b34bdce62da181
Anita Kuno <ay.kuno@gmail.com>
2011-05-11 13:48:00 -0400

    the spec and class for sort belong in this branch

---
+++ spec/sort_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require File.join(File.dirname(__FILE__), '..','lib', 'sort')
+
+#3) sort a list
+#       1) each of the nodes in the List will have a number (n > 0 < n)
+#       2) this method will re-order a copy of the list
+#       3) hint => use message 'dup' to make a copy of an object
+
+
+describe Sort do
+
+end
