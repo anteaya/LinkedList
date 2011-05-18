d00f3bcea9325d38060cdf8039375c7c587dcbee
Anita Kuno <ay.kuno@gmail.com>
2011-05-18 14:53:34 -0400

    making the spec more explicit about what kind of object it takes and what kind of object it returns

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,7 +4,7 @@ describe Node do
   include SpecHelper
 
   context "#new" do
-    it "takes a string or integer and returns a Node" do
+    it "initializes a new Node object with an argument(string or integer) and returns a node" do
       node_with_string_value = Node.new('string')
       node_with_integer_value = Node.new(78)
       node_with_string_value.should be_an_instance_of(Node)
@@ -28,7 +28,7 @@ describe Node do
     end
   end
 
-  it "#data should take a Node and return a string or integer(Fixnum)" do
+  it "#data takes a node and returns a string or integer(Fixnum)" do
     node = create_node(['date'])
     integer_node = create_node([56])
     node.data.should be_an_instance_of(String)
@@ -37,40 +37,57 @@ describe Node do
     integer_node.data.should == 56
   end
 
-  it "#the_next takes an instance of Node and returns an instance of Node" do
+  it "#the_next takes a node and returns the following node" do
     node = create_node(['apple', 'orange'])
     node.the_next.should be_an_instance_of(Node)
+    node.data.should == 'apple'
+    node.the_next.data.should == 'orange'
   end
 
   context "#insert_next" do
-    it "takes a string or an integer and returns an instance of Node" do
+    it "takes a node with argument(node) and returns the node that was passed in as an argument" do
       node = create_node(['guava'])
-      node.insert_next('chokecherry').should be_an_instance_of(Node)
+      return_value = node.insert_next(Node.new('chokecherry'))
+      return_value.should be_an_instance_of(Node)
+      return_value.data.should == 'chokecherry'
       integer_node = create_node([47])
-      integer_node.insert_next(90).should be_an_instance_of(Node)
+      integer_return_value = integer_node.insert_next(Node.new(90))
+      integer_return_value.should be_an_instance_of(Node)
+      integer_return_value.data.should == 90
     end
 
     it "has the correct value for new node" do
       node = create_node(['papaya'])
-      node.insert_next('pear').should be_an_instance_of(Node)
+      node.insert_next(Node.new('pear')).should be_an_instance_of(Node)
       node.the_next.data.should == 'pear'
     end
 
     it "inserts a node between two nodes" do
       node = create_node(['papaya', 'quince'])
-      node.insert_next('rhubarb')
-      node.the_next.the_next.data.should == 'quince' 
+      node.insert_next(Node.new('rhubarb'))
+      node.the_next.the_next.data.should == 'quince'
+    end
+
+    it "raises an error when the argument is not an instance of Node" do
+     lambda do
+        Node.new('plum').insert_next('shoe')
+      end.should raise_error
     end
   end
 
   context "#remove_next" do
-    it "takes an instance of Node and returns an instance of Node" do
+    it "takes a node and returns the removed node" do
       node = create_node(['pumpkin', 'squash'])
-      node.remove_next.should be_an_instance_of(Node)
+      return_value = node.remove_next
+      return_value.should be_an_instance_of(Node)
+      return_value.data.should == 'squash'
     end
-    it "removes next" do
+
+    it "removes the node following the reciever" do
       node = create_node(['starfruit', 'elderberry', 'gooseberry', 'cranberry'])
-      node.remove_next.data.should == 'elderberry'
+      node.data.should == 'starfruit'
+      node.the_next.data.should == 'elderberry'
+      node.remove_next
       node.the_next.data.should == 'gooseberry'
     end
   end
