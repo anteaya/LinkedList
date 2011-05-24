04f90f8cd386023abf49aaa4e57760c2da1f070d
Anita Kuno <ay.kuno@gmail.com>
2011-05-24 16:18:46 -0400

    ensured the create_node method works on a duplicate of the array passed in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'list')
 module SpecHelper
 
   def create_node(values)
+    values = values.dup
     nodes = nil
     while values.length != 0
       nodes = Node.new(values.last, nodes)
