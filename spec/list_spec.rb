98045d65749e64db93c4330b0922b61a30d22e1f
Anita Kuno <ay.kuno@gmail.com>
2011-05-24 16:22:11 -0400

    reworked a number of methods, specified the object returned for the methods, used the bang notation it identify which methods alter self

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -2,35 +2,52 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe List do
   include SpecHelper
+  
+  context "#new" do
+    it "initialzes" do
+      list = List.new
+      list.beginning_node.should == nil
+    end
 
-  it "initialzes" do
-    list = List.new
-    list.beginning_node.should == nil
+    it "takes a node and returns a list" do
+      node = Node.new('telephone')
+      list = List.new(node)
+      list.should be_an_instance_of(List)
+    end
   end
 
-  it "beginning node is nil if empty" do
-    list = List.new
-    list.beginning_node == nil
+  context "#beginning_node" do
+    it "is nil if empty" do
+      list = List.new
+      list.beginning_node == nil
+    end
+
+    it "takes a list and returns the beginning node" do
+      list = List.new(create_node(%w/lemon/))
+      list.beginning_node.should be_an_instance_of(Node)
+      list.beginning_node.data == 'lemon'
+    end
   end
 
-  it "beginning_node returns beginning node" do
-    list = List.new(create_node(['lemon']))
-    list.beginning_node.should be_an_instance_of(Node)
-    list.beginning_node.data == 'lemon'
-  end
-
-  it "each" do
-   fruits = ['cherimoya', 'avocado', 'soursop', 'persimmion', 'citron']
-   list = List.new(create_node(['cherimoya', 'avocado', 'soursop', 'persimmion', 'citron']))
+  it "#each takes a list and returns the same list" do
+   fruits = %w/cherimoya avocado soursop persimmion citron/
+   list = List.new(create_node(fruits))
+   before_id = list.object_id
    i = 0
    list.each do |x|
      x.data.should == (fruits[i])
      i += 1
    end
-   i.should == 5 
+   i.should == 5
+   list.object_id.should == before_id
   end
 
-  context "size" do
+  context "#size" do
+    it "takes a list and returns an integer(Fixnum)" do
+      list = List.new(create_node(%w/maple oak hemlock beech/))
+      return_value = list.size
+      return_value.should be_an_instance_of(Fixnum)
+    end
     it "returns 0 for a List with no nodes" do
       list = List.new
       list.size.should == 0
@@ -42,120 +59,114 @@ describe List do
     end
 
     it "returns 5 for a List with five nodes" do
-      list = List.new(create_node(['lychee', 'coconut', 'fig', 'pomegranate', 'loquat'])) 
+      list = List.new(create_node(%w/lychee coconut fig pomegranate loquat/)) 
       list.size.should == 5
     end
   end
   
-  context "insert_beginning" do
+  context "#insert_beginning!" do
+    it "takes a list with an argument(node) and returns the node passed in as an argument" do
+      list = List.new
+      return_value = list.insert_beginning!(create_node(['hollyhock']))
+      return_value.should be_an_instance_of(Node)
+      return_value.data.should == 'hollyhock'
+    end
     it "inserts a beginning into a list without a beginning" do
       list = List.new
-      list.insert_beginning(create_node(['grapefruit']))
+      list.insert_beginning!(create_node(['grapefruit']))
       list.beginning_node.data.should == 'grapefruit'
     end
 
     it "inserts a new beginning into a list with a beginning" do
       list = List.new(create_node(['apricot']))
-      list.insert_beginning(create_node(['kumquat']))
+      list.insert_beginning!(create_node(['kumquat']))
       list.beginning_node.data.should == 'kumquat'
       list.beginning_node.the_next.data.should == 'apricot'
     end
+  end
 
-    it "inserts a new beginning into a list as a specific location in the List" do
-      array = ['carrot', 'tomato', 'celery', 'bok choy']
-      node = create_node(['carrot', 'tomato', 'celery', 'bok choy'])
+  context "#reduce" do
+    it "duplicates a list and returns a smaller version, beginning with a specific location in the list" do
+      array = %w/carrot tomato celery spinach/
+      node = create_node(array)
       list = List.new(Node.new('eggplant', Node.new('beans', Node.new('peas', Node.new('potato', node)))))
-      list_with_new_beginning = list.insert_beginning(5)
+      reduced_list = list.reduce(5)
       i = 0
-      list_with_new_beginning.each do |x|
-        x.data.should == array[i]
-        i += 1
-      end
+        reduced_list.each do |x|
+          x.data.should == array[i]
+          i += 1
+        end
       i.should == 4
+      reduced_list.object_id.should_not == list.object_id
+    end
+
+    it "throws an error is argument is not an integer or if integer doesn't correspond to a node in the list" do
+      list = List.new(Node.new('plum', Node.new('prune', Node.new('raisin'))))
+      lambda do
+        list.reduce('apple')
+      end.should raise_error("reduce needs the location of a node expressed as an integer")
+
+      lambda do
+        list.reduce(6)
+      end.should raise_error("reduce needs the location of a node expressed as an integer")
     end
   end
 
-  context "remove_beginning" do
+  context "#remove_beginning!" do
+    it "takes a list and returns the removed node" do
+      list = List.new(create_node(%w/tulip lilac daffodil/))
+      return_value = list.remove_beginning!
+      return_value.should be_an_instance_of(Node)
+      return_value.data.should == 'tulip'
+    end
+
     it "from List with one node" do
       list = List.new(create_node(['watermelon']))
-      list.remove_beginning
+      list.remove_beginning!
       list.beginning_node.should == nil
     end
 
     it "from List with more than one node" do
       list = List.new(create_node(['strawberry', 'lime']))
-      list.remove_beginning
+      list.remove_beginning!
       list.beginning_node.data.should == 'lime'
     end
   end
 
-  context "remove_end" do
-    it "with no argument it removes the node at the end of the list" do
-      array = ['almond', 'walnut', 'peanut', 'pecan', 'hazelnut']  
-      list = List.new(create_node(['almond', 'walnut', 'peanut', 'pecan', 'hazelnut']))
-      shorter_list = list.remove_end
+  context "#remove_end!" do
+    it "takes a list and returns the removed node" do
+      list = List.new(create_node(%w/rose sorrel shrub/))
+      return_value = list.remove_end!
+      return_value.should be_an_instance_of(Node)
+      return_value.data.should == 'shrub'
+    end
+
+    it "removes the node at the end of the list" do
+      array = %w/almond walnut peanut pecan hazelnut/  
+      list = List.new(create_node(array))
+      list.remove_end!
       i = 0
-      shorter_list.each do |x|
+      list.each do |x|
         x.data.should == array[i]
         i += 1
       end
       i.should == 4
     end
 
-    it "with a location argument it removes the remaining nodes in the list" do
-      array = ['red', 'orange', 'yellow']
-      list = List.new(create_node(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']))
-      list_with_three_nodes = list.remove_end(3)
-      i = 0
-      list_with_three_nodes.each do |x|
-        x.data.should == array[i]
-        i += 1
-      end
-      i.should == 3
-    end
   end
 
-  context "#split" do
-    it "returns two lists accounting for all the nodes from a list of even size" do
-      left_array = ['bonnet', 'bowler', 'Stetson', 'fedora']
-      right_array = ['hat', 'chapeau', 'cap', 'lid']
-      node = create_node(['hat', 'chapeau', 'cap', 'lid'])
-      list = List.new(Node.new('bonnet', Node.new('bowler', Node.new('Stetson', Node.new('fedora', node)))))
-      left, right = list.split
+  context "#truncate_to_end" do
+    it "takes a list with a location argument, duplicates the list, removes the remaining nodes and returns the duplicate list" do
+      array = %w/red orange yellow green blue indigo violet/
+      list = List.new(create_node(array))
+      returned_list = list.truncate_to_end(3)
       i = 0
-      left.each do |x|
-        x.data.should == left_array[i]
-        i += 1
-      end
-      i.should == 4
-      j = 0
-      right.each do |x|
-        x.data.should == right_array[j]
-        j += 1
-      end
-      j.should == 4
-      right.beginning_node.object_id.should == node.object_id
-    end
-
-    it "returns two lists accounting for all the nodes from a list of uneven size" do
-      left_array = ['wimple', 'shawl', 'babushka']
-      right_array = ['miter', 'tiara', 'toque', 'turban']
-      node = create_node(['miter','tiara','toque','turban'])
-      list = List.new(Node.new('wimple', Node.new('shawl', Node.new('babushka', node))))
-      left, right = list.split
-      i = 0
-      left.each do |x|
-        x.data.should == left_array[i]
+      list.each do |x|
+        x.data.should == array[i]
         i += 1
       end
       i.should == 3
-      j = 0
-      right.each do |x|
-        x.data.should == right_array[j]
-        j += 1
-      end
-      j.should == 4
-      right.beginning_node.object_id.should == node.object_id
+      returned_list.object_id.should_not == list.object_id
     end
   end
 end
