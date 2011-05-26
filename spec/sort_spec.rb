fa46f1151f16f698d04daf8a2aaeb73f18a185f0
Anita Kuno <ay.kuno@gmail.com>
2011-05-26 17:11:51 -0400

    use the copy method elsewhere in the List methods, removed spec for sort method, updated spec for swap and split method, rewrote split method

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -3,51 +3,75 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe List do
   include SpecHelper
 
-  context "sort" do
-    it "creates a duplicate of List" do
-      list = List.new(create_node([ 88, 4, 901, 43, 26, 78, 1, 9 ]))
-      list.each {|x| x.inspect}.object_id.should == list.object_id
-      list.sort.object_id.should_not == list.object_id
-    end
-
-    it "sorts a List with one node" do
-      list = List.new(create_node([5]))
+  context "#swap!" do
+    it "takes a list of maximum size 2 and swaps the nodes and returns the original list" do
+      node = create_node(['cat'])
+      list = List.new(Node.new('horse', node))
+      before_id = list.object_id
+      list.swap!
+      list.beginning_node.object_id.should == node.object_id
       i = 0
-      list.sort.each do |node|
-        node.data.should == 5
+      list.each do |x|
+        x.data.should == ['cat', 'horse'][i]
         i += 1
       end
-      i.should == 1
-    end
+      i.should == 2
+      list.object_id.should == before_id
 
- #  it "sorts a List with two nodes" do
- #    array = [2, 8]
- #    list = List.new(create_node([8, 2]))
- #    i = 0
- #    list.sort.each do |node|
- #      node.data.should == array[i]
- #      i += 1
- #    end
- #    i.should == 2
- #  end
+      lambda do
+        List.new(Node.new('Panama', Node.new('boater', Node.new('veil')))).swap
+      end.should raise_error
+    end
   end
 
-  context "swap" do
-    it "takes a list of maximum size 2 and swaps the nodes" do
-    node = create_node(['cat'])
-    list = List.new(Node.new('horse', node))
-    list.swap
-    list.beginning_node.object_id.should == node.object_id
-    i = 0
-    list.each do |x|
-      x.data.should == ['cat', 'horse'][i]
-      i += 1
+  context "#split" do
+    it "takes a list and returns two new lists" do
+      array = %w/pie cake pudding hat coat/
+      list = List.new(create_node(array))
+      left, right = list.split
+      left.should be_an_instance_of(List)
+      right.should be_an_instance_of(List)
     end
-    i.should == 2
 
-    lambda do
-      List.new(Node.new('Panama', Node.new('boater', Node.new('veil')))).swap
-    end.should raise_error
+    it "returns two lists accounting for all the nodes from a list of even size" do
+      right_array = ['hat', 'chapeau', 'cap', 'lid']
+      node = create_node(right_array)
+      left_array = ['bonnet', 'bowler', 'Stetson', 'fedora']
+      array = left_array + right_array
+      list = List.new(create_node(array))
+      left, right = list.split
+      i = 0
+      left.each do |x|
+        x.data.should == left_array[i]
+        i += 1
+      end
+      i.should == 4
+      j = 0
+      right.each do |x|
+        x.data.should == right_array[j]
+        j += 1
+      end
+      j.should == 4
+    end
+
+    it "returns two lists accounting for all the nodes from a list of uneven size" do
+      left_array = ['wimple', 'shawl', 'babushka']
+      right_array = ['miter', 'tiara', 'toque', 'turban']
+      node = create_node(right_array)
+      list = List.new(Node.new('wimple', Node.new('shawl', Node.new('babushka', node))))
+      left, right = list.split
+      i = 0
+      left.each do |x|
+        x.data.should == left_array[i]
+        i += 1
+      end
+      i.should == 3
+      j = 0
+      right.each do |x|
+        x.data.should == right_array[j]
+        j += 1
+      end
+      j.should == 4
     end
   end
 end
